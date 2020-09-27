7c95a3271245236ecac7e8b2f1ca66df55f74eee
Lika <borisovalika2@mail.com>
2020-09-28 01:40:16 +0300

    feat: make carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
+// jshint esversion:6
 const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
+  let k;
+  let t;
+  if (typeof String(sampleActivity) || /[a-zA-Z]/.test(sampleActivity)) return false;
+
+  k = 0.693 / HALF_LIFE_PERIOD;
+  t = parseInt(Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / k));
+
+  return t;
+
 };
