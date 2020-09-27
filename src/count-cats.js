5a528359bc77a4aeaebad725e36637c7cc873f42
Lika <borisovalika2@mail.com>
2020-09-27 23:42:37 +0300

    feat: make count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
+//jshint esversion:6
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  // throw new CustomError('Not implemented');
+  let count = 0;
+  if (arr === []) return 0;
+  arr.forEach(element => {  
+    element.forEach(item => {
+      if (item === "^^") {
+        count++;
+      }
+    });
+  });
+  return count;
 };
