34b6f625556090a65a716c9acea052f7c92071c6
Lika <borisovalika2@mail.com>
2020-09-28 04:01:23 +0300

    feat: make what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,27 @@
+// jshint esversion:6
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  try {
+    date.getTime();
+	} catch(e) { 
+    throw new Error(e); 
+  }
+  
+  let month = date.getMonth();
+	const season = {
+    	winter: {0: "January", 1: "February", 11: "December"},
+    	spring: {2: "March", 3: "April", 4: "May"},
+    	summer: {5: "June", 6: "July", 7: "August"},
+    	autumn: {8: "September", 9: "October", 10: "November"}
+  	};
+
+	for (const [key, value] of Object.entries(season)) {
+		for (const item in value) {
+			if (item == month) {
+        return key;
+			}
+		}
+	}
 };
