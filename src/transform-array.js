f5b6b8d6d2a271f1aa91dc9cf356a948884f162d
Lika <borisovalika2@mail.com>
2020-10-29 18:06:53 +0300

    feat: transform-array func

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,25 +2,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  // throw new CustomError('Not implemented');
-  let newArr = [...arr];
-  // console.log(newArr);
-  for (let i = 0; i < newArr.length; i++) {
 
-     
-  }
-  newArr.map((item, index, array) => {
-    if (item === "--discard-next") {
-      array.splice(array[index + 1], 1);
-    } else if (item === "--discard-prev") {
-      array.splice(array[index - 1], 1);
-    } else if (item === "--double-next") {
-      arr.splice(array[index + 1], 0, array[index + 1]);
-    } else if (item === "--double-prev") {
-      arr.splice(array[index - 1], 0, array[index - 1]);
+	if (!Array.isArray(arr)) throw new Error();
+
+	let newArr = arr.reduce((total, next, index) => {
+		
+  		if (next === '--discard-next' ||
+  			next === '--discard-prev' ||
+  			next === '--double-prev' ||
+  			next === '--double-next'
+  			) return total;
+
+		if (arr[index - 1] === '--discard-next') return total;
+		if (arr[index - 1] === '--double-next') {
+      total.push(next, next); 
+    } else {
+      total.push(next);
+    }
+		if (arr[index + 1] === '--double-prev') {
+      total.push(next);
+    }
+		if(arr[index + 1] === '--discard-prev') {
+      total.splice(total.length - 1, 1);
     }
 
-  });
+		return total;
 
-  return arr;
+	}, []);
+
+	return newArr;
 };
